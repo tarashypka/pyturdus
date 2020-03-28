f35df70560ad37f756981c8cd25ddd93132e009e
Taras Shypka <tarashypka@gmail.com>
2020-03-28 16:59:35 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 MODULE_DIR=$(realpath $(dirname $0))
-PYTHON_VERSION=3.7
 
-CONDA_PATH="missing"
-ENV_NAME="missing"
-HELP_MSG="Usage: install.sh --conda=/path/to/conda --env=env_name"
+ENV_PATH="missing"
+HELP_MSG="Usage: install.sh --env=env_path"
 
 err() {
     echo
@@ -19,11 +17,7 @@ err() {
 for param in $@; do
   case ${param} in
     --env=*)
-      ENV_NAME=${param#*=}
-      shift
-      ;;
-    --conda=*)
-      CONDA_PATH=${param#*=}
+      ENV_PATH=${param#*=}
       shift
       ;;
     --help)
@@ -32,29 +26,8 @@ for param in $@; do
   esac
 done
 
-if [[ ${CONDA_PATH} == "missing" ]]; then
-  err "Not found --conda argument!"
-fi
-
-if [[ ${ENV_NAME} == "missing" ]]; then
+if [[ ${ENV_PATH} == "missing" ]]; then
   err "Not found --env argument!"
 fi
 
-echo ENV=${ENV_NAME}
-echo CONDA=${CONDA_PATH}
-
-export PATH=${CONDA_PATH}/bin:$PATH
-
-ENV_PATH=${CONDA_PATH}/envs/${ENV_NAME}
-if [[ ! -d ${ENV_PATH} ]]; then
-    echo "Create new environment at ${ENV_PATH} ..."
-    conda create -n ${ENV_NAME} python=${PYTHON_VERSION}
-    ENV_INSTALLED=1
-fi
-conda activate ${ENV_NAME}
-
-PIP=${ENV_PATH}/bin/pip
-
-cd ${MODULE_DIR}
-
-${PIP} install -r requirements.txt
+${ENV_PATH}/bin/pip install -r ${MODULE_DIR}/requirements.txt
