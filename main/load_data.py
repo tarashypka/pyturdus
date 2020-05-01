9660c628c6587c6121537e226297a8ed1b47b0b2
Taras Shypka <tarashypka@gmail.com>
2020-05-01 17:28:16 +0300

    update load_data.py

--- main/load_data.py
+++ main/load_data.py
@@ -6,7 +6,7 @@ from typing import Dict
 import pandas as pd
 from tqdm import tqdm
 
-sys.path.insert(0, '/home/tas/Workspace/python/pysimple/src')
+sys.path.insert(0, '/home/tas/Workspace/pysimple/src')
 from pysimple.io import from_tsv, to_tsv, ensure_filedir, ensure_dir
 
 
@@ -37,30 +37,24 @@ def format_path(path: Path, **kwargs) -> Path:
 
 
 def get_records(page: int) -> Dict:
-    """Using xeno-canto API get records"""
+    """Get bird record data from xeno-canto API"""
     return requests.get(RECORDS_API.format(page=page)).json()
 
 
 def download_call(id: int):
-    """Download call from xeno-canto"""
+    """Download bird call data from xeno-canto"""
     return requests.get(CALLS_API.format(call_id=id)).content
 
 
-def load_gens() -> pd.DataFrame:
-    """Load records for most popular species"""
-    records = from_tsv(RECORDS_PATH, usecols=['q', 'gen', 'id'])
-    # Get only best quality records
-    records = records[records['q'].isin(QUALITY)]
-    # Get only most popular birds
-    gens = records['gen'].value_counts().index[:MAX_GENS]
-    return records[records['gen'].isin(gens)]
-
-
 def load_records():
+    """
+    Download bird records data from xeno-canto API.
+    Download only new data, while keeping previously downloaded.
+    """
     pages = int(get_records(page=1)['numPages'])
     pages = min(pages, MAX_PAGES)
 
-    print(f'Load records from {pages} pages ...')
+    print(f'Load bird records from {pages} pages at xeno-canto ...')
     records = []
     for page in tqdm(range(1, pages+1)):
         page_records_path = ensure_filedir(format_path(PAGE_RECORDS_PATH, page=page))
@@ -78,22 +72,36 @@ def load_records():
     records = pd.concat(records)
     to_tsv(RECORDS_PATH, data=records)
     records = from_tsv(filepath=RECORDS_PATH)
-    print(f'Loaded {len(records)} records!')
+    print(f'Loaded {len(records)} bird records!')
 
+    print('Split loaded records into most popular species ...')
     for gen, _ in load_gens().groupby('gen'):
-        print(f'Load records for gen {gen} ...')
         gen_records_path = ensure_filedir(format_path(GEN_RECORDS_PATH, gen=gen))
         if gen_records_path.exists():
             continue
         gen_records = records[records['gen'].eq(gen)]
         to_tsv(filepath=gen_records_path, data=gen_records)
+        
+
+def load_gens() -> pd.DataFrame:
+    """Load records for most popular species that were previously downloaded from xeno-canto API"""
+    records = from_tsv(RECORDS_PATH, usecols=['q', 'gen', 'id'])
+    # Get only best quality records
+    records = records[records['q'].isin(QUALITY)]
+    # Get only most popular birds
+    gens = records['gen'].value_counts().index[:MAX_GENS]
+    return records[records['gen'].isin(gens)]
 
 
 def load_calls():
+    """
+    Download bird calls from xeno-canto for most popular species.
+    Download only new data, while keeping previously downloaded.
+    """
     print(f'Load calls for {MAX_GENS} gens ...')
     for gen, gen_records in load_gens().groupby('gen'):
         gen_record_ids = gen_records['id'].values[:MAX_CALLS]
-        print(f'Load {len(gen_record_ids)} calls for gen {gen} ...')
+        print(f'Load {len(gen_record_ids)} bird calls for specie {gen} ...')
         for call_id in tqdm(gen_record_ids):
             gen_call_path = ensure_filedir(format_path(GEN_CALL_PATH, gen=gen, call_id=call_id))
             if gen_call_path.exists():
