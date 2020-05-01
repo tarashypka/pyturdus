2f6b1e24998081e2656a7424fbca03c5a3555169
Taras Shypka <tarashypka@gmail.com>
2020-05-01 23:13:52 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -30,6 +30,13 @@ if [[ ${ENV_PATH} == "missing" ]]; then
   err "Not found --env argument!"
 fi
 
+echo "Install pysimple ..."
+rm -rf .cache/pysimple
+git clone https://github.com/tarashypka/pysimple.git .cache/pysimple
+cd .cache/pysimple
+/usr/bin/env bash install.sh --env=${ENV_PATH}
+cd -
+
 ${ENV_PATH}/bin/pip install -r ${MODULE_DIR}/requirements.txt
 
 # Dependencies required for Notebook plots
